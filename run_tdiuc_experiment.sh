92757e2c37b54359b3a030db4aebe6386053314d
qzhb <zhaobo.qi@vipl.ict.ac.cn>
2021-03-03 11:22:06 +0800

    更新代码

---
+++ run_tdiuc_experiment.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#source activate remind_proj
+
+lr=2e-3
+CONFIG=TDIUC_streaming
+export PYTHONPATH=/home/qzhb/dorren/CL4VQA/REMIND
+
+DATA_ORDER=qtype
+expt=${CONFIG}_${DATA_ORDER}_${lr}
+
+CUDA_VISIBLE_DEVICES=0 nohup python -u vqa_trainer.py \
+--config_name ${CONFIG} \
+--expt_name ${expt} \
+--stream_with_rehearsal \
+--data_order ${DATA_ORDER} \
+--lr ${lr} &> logs/${expt}.log &
+
+#DATA_ORDER=qtype # or qtype
+#expt=${CONFIG}_${DATA_ORDER}_${lr}
+
+#CUDA_VISIBLE_DEVICES=0 python -u vqa_experiments/vqa_trainer.py \
+#--config_name ${CONFIG} \
+#--expt_name ${expt} \
+#--stream_with_rehearsal \
+#--data_order ${DATA_ORDER} \
+#--lr ${lr}
